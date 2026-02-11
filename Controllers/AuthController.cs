fe2472fe81762d2c0be1a8b2ab0c68915bea142c
ebrar türüdü <ebrartrd@gmail.com>
2026-02-11 18:03:12 +0300

    feat: implement JWT authorization on ProductsController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,20 +2,27 @@ using Microsoft.AspNetCore.Mvc;
 using ProductApi.Application.DTOs;
 using ProductApi.Core.Entities;
 using ProductApi.Infrastructure.Persistence;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProductApi.Controllers;
-
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class AuthController : ControllerBase
 {
     private readonly AppDbContext _context;
-
-    public AuthController(AppDbContext context)
-    {
-        _context = context;
-    }
-
+    private readonly IConfiguration _configuration;
+    public AuthController(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration; 
+        }
+    
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterDto request)
     {
@@ -30,4 +37,44 @@ public class AuthController : ControllerBase
 
         return Ok("Kullanıcı başarıyla oluşturuldu.");
     }
+    [HttpPost("login")]
+public async Task<IActionResult> Login(LoginDto request)
+{
+    var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+    if (user == null) return BadRequest("Kullanıcı bulunamadı.");
+
+    if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+    {
+        return BadRequest("Hatalı şifre.");
+    }
+
+    var token = CreateToken(user);
+
+    return Ok(new { token });
+}
+
+private string CreateToken(User user)
+{
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, user.Username),
+        new Claim(ClaimTypes.Role, user.Role),
+        new Claim("Id", user.Id.ToString())
+    };
+
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+        _configuration.GetSection("Jwt:Key").Value!));
+
+    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+    var tokenDescriptor = new JwtSecurityToken(
+        issuer: _configuration.GetSection("Jwt:Issuer").Value,
+        audience: _configuration.GetSection("Jwt:Audience").Value,
+        claims: claims,
+        expires: DateTime.Now.AddDays(1), 
+        signingCredentials: creds
+    );
+
+    return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+}
 }
