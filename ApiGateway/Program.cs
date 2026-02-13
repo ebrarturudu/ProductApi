813bd65a3c1afb30c6acb703e4c68a24855bc725
ebrar türüdü <ebrartrd@gmail.com>
2026-02-13 22:49:36 +0300

    feat(gateway): implement rate limiting using YARP

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.AspNetCore.RateLimiting; 
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.Window = TimeSpan.FromSeconds(10); 
+        opt.PermitLimit = 5; 
+        opt.QueueLimit = 0; 
+    });
+});
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
@@ -9,6 +22,7 @@ builder.Services.AddReverseProxy()
 
 var app = builder.Build();
 
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
